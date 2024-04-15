6f28cd0afabc300dd9dc14acc80c7c65cd930af0
srjames-ukho <126559680+srjames-ukho@users.noreply.github.com>
2024-04-15 09:01:04 +0100

    adding project for web app

---
+++ azure-monitoring/WeatherForecastHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace azure_monitoring;
+
+public static class WeatherForecastHandler
+{
+    public static readonly string Pattern = "/weatherforecast";
+    public static readonly string Name = "GetWeatherForecast";
+    
+    private static readonly string[] Summaries =
+    [
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
+
+    public static readonly Delegate Handler = () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    Summaries[Random.Shared.Next(Summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    };
+
+    private record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
